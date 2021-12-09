b765b3aac05376fd7ee86f893e88f236abe0c31e
Chase Crowder <gcrowder93@gmail.com>
2021-12-08 18:02:56 -0800

    added styles

--- src/Components/PokeList/PokeList.js
+++ src/Components/PokeList/PokeList.js
@@ -13,24 +13,31 @@ export default function PokeList({
   };
   {
     return (
-      <div>
+      <div className="grid">
         {pokemon.map((p) => (
           <>
-            <p key={p.id}>
-              {p.pokemon}
+            <p className="pokename" key={p.id}>
+              {p.pokemon.charAt(0).toUpperCase() + p.pokemon.slice(1)}
+
+              <img src={p.url_image}></img>
               <p key={p.id}>
-                {p.type_1} {p.type_2}
+                Type 1-{p.type_1} Type 2-{p.type_2}
                 <p key={p.id}>
-                  {p.hp} {p.attack} {p.defence} {p.special_attack} {p.special_defence} {p.speed}
-                  <p key={p.id}>{p.species_id}</p>
+                  {/* {p.hp} {p.attack} {p.defence} {p.special_attack} {p.special_defence} {p.speed} */}
+                  <p key={p.id}>#{p.species_id}</p>
                 </p>
               </p>
             </p>
           </>
         ))}
-        <div> Page: {currentPage}</div>
+        <div>
+          {' '}
+          Page: {currentPage} <br></br>
+        </div>
         {/* <button onClick={() => setCurrentPage}>Next Page</button> */}
-        <button onClick={() => setCurrentPage((prevState) => ++prevState)}>Next Page</button>
+        <button className="button2" onClick={() => setCurrentPage((prevState) => ++prevState)}>
+          Next Page
+        </button>
       </div>
     );
   }
