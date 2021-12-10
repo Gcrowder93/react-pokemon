b629450e1a488821562f69ab3b3c8b26c7493e8e
Chase Crowder <gcrowder93@gmail.com>
2021-12-09 17:31:02 -0800

    wrote test for snapshot, couldnt get the snapshot to take

---
+++ src/Components/PokeList/PokeList.test.js
@@ -0,0 +1,7 @@
+import { render } from '@testing-library/react';
+import PokeList from './PokeList';
+
+test('renders the pokemon name', () => {
+  const container = render(<PokeList poke="name" />);
+  expect(container).toMatchSnapshot();
+});
