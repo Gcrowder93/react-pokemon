1da88fb82200176b421abf8d44f4509237d4833a
Chase Crowder <gcrowder93@gmail.com>
2021-12-10 11:44:33 -0800

    THESE DAMN TESTS

--- src/Components/PokeList/PokeList.js
+++ src/Components/PokeList/PokeList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button } from '@mui/material';
 export default function PokeList({ pokemon, currentPage, setCurrentPage, setLoading }) {
+  console.log(pokemon);
   const handleNextPage = () => {
     setCurrentPage((prevState) => ++prevState);
     setLoading(true);
@@ -10,28 +11,22 @@ export default function PokeList({ pokemon, currentPage, setCurrentPage, setLoad
       <div className="container">
         <div className="grid">
           {pokemon.map((p) => (
-            <>
-              <p className="pokename" key={p.id}>
-                {p.pokemon.charAt(0).toUpperCase() + p.pokemon.slice(1)}
-                <img src={p.url_image}></img>
-                <p key={p.id}>
-                  Type1:{p.type_1.charAt(0).toUpperCase() + p.type_1.slice(1)} Type2:
-                  {p.type_2.charAt(0).toUpperCase() + p.type_2.slice(1)}
-                  <p key={p.id}>
-                    HP:{p.hp} ATT:{p.attack} DEF:{p.defense}
-                    <div>
-                      <br></br>
-                    </div>{' '}
-                    {/* Spec. Att: {p.special_attack} Spec. Def: {p.special_defence} */}
-                    {/* <div>
+            <div className="pokename" key={p._id}>
+              console.log(p._id)
+              {p.pokemon.charAt(0).toUpperCase() + p.pokemon.slice(1)}
+              <img src={p.url_image}></img>
+              <div>
+                Type1:{p.type_1.charAt(0).toUpperCase() + p.type_1.slice(1)} Type2:
+                {p.type_2.charAt(0).toUpperCase() + p.type_2.slice(1)}
+                HP:{p.hp} ATT:{p.attack} DEF:{p.defense}
+                <br></br> {/* Spec. Att: {p.special_attack} Spec. Def: {p.special_defence} */}
+                {/* <div>
                     <br></br>
                   </div>{' '}
                   Speed {p.speed} */}
-                    <p key={p.id}>#{p.species_id}</p>
-                  </p>
-                </p>
-              </p>
-            </>
+                <p>#{p.species_id}</p>
+              </div>
+            </div>
           ))}
         </div>
         <div className="pagenumber">Page: {currentPage}</div>
--- src/Components/PokeList/PokeList.test.js
+++ src/Components/PokeList/PokeList.test.js
@@ -1,7 +1,13 @@
-// import { render } from '@testing-library/react';
-// import PokeList from './PokeList';
+import { render } from '@testing-library/react';
+import PokeList from './PokeList';
 
-// test('renders the pokemon name', () => {
-//   const container = render(<PokeList poke="name" />);
-//   expect(container).toMatchSnapshot();
-// });
+test('renders the pokemon name', () => {
+  const Charmander = {
+    pokemon: 'Charmander',
+    type_1: 'Fire',
+    type_2: 'NA',
+    _id: 1,
+  };
+  const container = render(<PokeList pokemon={[Charmander]} />);
+  expect(container).toMatchSnapshot();
+});
